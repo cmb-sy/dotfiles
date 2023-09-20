049f43e829cb476bce8eee38d6833c0482c392a3
nakashima1125 <snakashima@armg.jp>
2023-09-20 13:25:36 +0900

    add json file

--- vscode/copy_of_settings.json_with_comments.rb
+++ vscode/copy_of_settings.json_with_comments.rb
@@ -52,4 +52,45 @@
   
   // アクティビティバー（左側のサイドバー）を非表示にするかどうかを設定
   "workbench.activityBar.visible": false
+
+  {
+  "editor.accessibilityPageSize": 13,
+  "editor.fontSize": 20,
+  "terminal.integrated.fontSize": 18,
+  "scm.inputFontSize": 15,
+  "liveServer.settings.donotShowInfoMsg": true,
+
+  "[python]": {
+    "editor.defaultFormatter": "ms-python.black-formatter",
+    "editor.formatOnSave": true
+  },
+  "python.defaultInterpreterPath":"Python\\Python310\\python.exe",
+  "explorer.confirmDelete": false,
+  "r.rterm.windows": "Programs\\R\\R-4.2.3\\bin\\R.exe",
+  "r.rpath.windows": "Programs\\R\\R-4.2.3\\bin\\R.exe",
+
+  "[quarto]": {
+    "editor.wordWrap": "on",
+    "editor.quickSuggestions": {
+      "comments": "on",
+      "other": "on",
+      "strings": "on"
+    },
+    "editor.quickSuggestionsDelay": 250,
+    "editor.snippetSuggestions": "none",
+    "editor.wordBasedSuggestions": false,
+    "editor.suggestOnTriggerCharacters": true,
+    "editor.unicodeHighlight.ambiguousCharacters": false,
+    "editor.unicodeHighlight.invisibleCharacters": false,
+    "editor.defaultFormatter": "ms-python.black-formatter",
+    "editor.formatOnSave": true,
+  },
+  "python.analysis.extraPaths": ["\\venv\\Lib\\site-packages"],
+  "terminal.integrated.env.windows": {
+    "PSExecutionPolicyPreference": "RemoteSigned"
+  },
+  "explorer.confirmDragAndDrop": false,
+  "git.path": "",
+}
+
 }
