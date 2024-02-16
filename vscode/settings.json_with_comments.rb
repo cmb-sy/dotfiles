70ecbd1b967fd02c64815cc69648444e1a4a532a
cmb-sy <free.nakashima@gmail.com>
2024-02-16 16:13:15 +0900

    rename file

---
+++ vscode/settings.json_with_comments.rb
@@ -0,0 +1,96 @@
+{
+  //制御文字（タブ、改行など）を表示するかどうか
+  "editor.renderControlCharacters": true,
+  
+  //エディタの提案候補の最初の候補を自動的に選択するように設定
+  "editor.suggestSelection": "first",
+  
+  //ファイルの最後に自動的に改行を挿入するかどうかを設定
+  "files.insertFinalNewline": true,
+  
+  // フォントの合字 (ligatures) を有効にするかどうかを設定
+  "editor.fontLigatures": true,
+  
+  // エディタのフォントサイズを設定
+  "editor.fontSize": 16,
+  
+  // 選択した行を強調表示する方法を設定
+  "editor.renderLineHighlight": "all",
+  
+  // カーソルの点滅のスタイルを設定
+  "editor.cursorBlinking": "smooth",
+  
+  // カーソルの移動時に滑らかなアニメーションを有効にするかどうかを設定
+  "editor.cursorSmoothCaretAnimation": "on",
+  
+  // ファイルのエンコーディングを自動的に推測するかどうかを設定
+  "files.autoGuessEncoding": true,
+  
+  // ウィンドウのズームレベルを設定
+  "window.zoomLevel": 0,
+  
+  // 対応する括弧の色付けを有効にするかどうかを設定
+  "editor.bracketPairColorization.enabled": true,
+  
+  // 統合ターミナルのフォントサイズを設定
+  "terminal.integrated.fontSize": 16,
+  
+  // ファイルを保存するときに自動的にフォーマットを適用するかどうかを設定
+  "editor.formatOnSave": true,
+  
+  // カラーテーマを設定
+  "workbench.colorTheme": "GitHub Dark Default",
+  
+  // サイドバーのミニマップを無効にするかどうかを設定
+  "editor.minimap.enabled": false,
+  
+  // パンくずリストを無効にするかどうかを設定
+  "breadcrumbs.enabled": false,
+  
+  // メニューバーの表示/非表示を切り替える方法を設定
+  "window.menuBarVisibility": "toggle",
+  
+  // アクティビティバー（左側のサイドバー）を非表示にするかどうかを設定
+  "workbench.activityBar.visible": false
+
+  {
+  "editor.accessibilityPageSize": 13,
+  "editor.fontSize": 20,
+  "terminal.integrated.fontSize": 18,
+  "scm.inputFontSize": 15,
+  "liveServer.settings.donotShowInfoMsg": true,
+
+  "[python]": {
+    "editor.defaultFormatter": "ms-python.black-formatter",
+    "editor.formatOnSave": true
+  },
+  "python.defaultInterpreterPath":"Python\\Python310\\python.exe",
+  "explorer.confirmDelete": false,
+  "r.rterm.windows": "Programs\\R\\R-4.2.3\\bin\\R.exe",
+  "r.rpath.windows": "Programs\\R\\R-4.2.3\\bin\\R.exe",
+
+  "[quarto]": {
+    "editor.wordWrap": "on",
+    "editor.quickSuggestions": {
+      "comments": "on",
+      "other": "on",
+      "strings": "on"
+    },
+    "editor.quickSuggestionsDelay": 250,
+    "editor.snippetSuggestions": "none",
+    "editor.wordBasedSuggestions": false,
+    "editor.suggestOnTriggerCharacters": true,
+    "editor.unicodeHighlight.ambiguousCharacters": false,
+    "editor.unicodeHighlight.invisibleCharacters": false,
+    "editor.defaultFormatter": "ms-python.black-formatter",
+    "editor.formatOnSave": true,
+  },
+  "python.analysis.extraPaths": ["\\venv\\Lib\\site-packages"],
+  "terminal.integrated.env.windows": {
+    "PSExecutionPolicyPreference": "RemoteSigned"
+  },
+  "explorer.confirmDragAndDrop": false,
+  "git.path": "",
+}
+
+}
