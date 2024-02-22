16aae271c5102b3cedff20c17df25fe01aed42ed
cmb-sy <free.nakashima@gmail.com>
2024-02-22 15:03:34 +0900

    update

--- zsh/apply_zsh.zsh
+++ zsh/apply_zsh.zsh
@@ -1,9 +1,11 @@
 DOT_DIR="${HOME}/dotfiles/zsh"
 
-
 echo  "Create zsh links ..."
 
 ln -snfv "${DOT_DIR}/.zshenv" "${HOME}/.zshenv"
 ln -snfv "${DOT_DIR}/.zshrc" "${HOME}/.zshrc"
 ln -snfv "${DOT_DIR}/.aliases.sh" "${HOME}/.aliases"
 ln -snfv "${DOT_DIR}/.functions.zsh" "${HOME}/.functions.zsh"
+
+# sheldon init --shell zsh
+ln -snfv "${DOT_DIR}/.config/sheldon/.plugin.toml" "${HOME}/.config/sheldon/plugins.toml"
