2b2dee03cf6841d74737a7e1bb11744f53341001
cmb-sy <free.nakashima@gmail.com>
2025-03-26 16:01:42 +0900

    update

---
+++ setup/brew_install.sh
@@ -0,0 +1,67 @@
+#!/bin/zsh -eux
+
+export HOMEBREW_NO_AUTO_UPDATE=1
+
+# command isntall
+formulas=(
+echo "installing homebrew..."
+which brew >/dev/null 2>&1 || /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+echo "run brew doctor..."
+which brew >/dev/null 2>&1 && brew doctor
+
+echo "run brew update..."
+which brew >/dev/null 2>&1 && brew update
+
+echo "run brew upgrade..."
+brew upgrade
+
+formulas=(
+    fzf
+    sheldon
+    git
+    miniconda
+    nodebrew
+    yarn
+    gengig
+    tsc
+)
+
+# GUI application install
+casks=(
+  docker
+  github
+  google-chrome
+  microsoft-teams
+  slack
+  sourcetree
+  visual-studio-code
+  zoom
+)
+
+brew update
+brew tap homebrew/cask-fonts
+brew tap ktr0731/evans
+brew tap jesseduffield/lazydocker
+
+for formula in ${formulas[@]}; do
+  brew install ${formula}
+done
+
+eval "$(starship init zsh)"
+case "${OSTYPE}" in
+darwin*)
+  for cask in ${casks[@]}; do
+    brew install --cask ${cask}
+  done
+
+  GCC_VER=$(ls $(brew --prefix)/bin | grep -E "^g\+\+\-(\d+) \->" | awk '{print $1}' | sed -e "s/g++-//g")
+  sudo ln -snfv $(brew --prefix gcc)/gcc-${GCC_VER} /usr/local/bin/gcc
+  sudo ln -snfv $(brew --prefix gcc)/g++-${GCC_VER} /usr/local/bin/g++
+  ;;
+linux*)
+  brew install docker
+  ;;
+esac
+
+unset HOMEBREW_NO_AUTO_UPDATE
