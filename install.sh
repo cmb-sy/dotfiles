ea9af0d6b5f11ff3d871fe9ad796719298953d14
kunst <63276819+cmb-sy@users.noreply.github.com>
2025-04-02 09:06:05 +0900

    不要なファイルの削除

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# This is a wrapper script that calls the main installation script in the setup directory
-# It exists for backwards compatibility with CI workflows
-
-echo "Running main install script at ./setup/install.sh"
-./setup/install.sh
